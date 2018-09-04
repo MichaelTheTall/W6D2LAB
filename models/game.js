6da8af2763a7253cefe0b2dd5a1e9f5f01d510a3
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-04 15:01:35 +0100

    classes

--- models/game.js
+++ models/game.js
@@ -1,3 +1,15 @@
-const Game = function() {
-
+const Game = function(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+  this.allcards = [card1,card2,card3,card4,card5,card6];
 }
+
+Game.prototype.deal = function () {
+  this.player1.cards = [card1,card3,card5];
+  this.player2.cards = [card2,card4,card6];
+};
+  let lastplayer;
+  let wonlastturn;
+Game.prototype.turn = function () {
+  
+};
